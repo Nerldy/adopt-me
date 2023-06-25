948224db04550bfb960e448cb99b1f99b9ee3a7c
nerldy <arnodhis@gmail.com>
2023-06-25 07:20:27 +0300

    configure eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,54 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
+// const animals = [
+//   {
+//     name: "Luna",
+//     animal: "Dog",
+//     breed: "Havanesse",
+//   },
+//   {
+//     name: "Pepper",
+//     animal: "Bird",
+//     breed: "Cockateil",
+//   },
+//   {
+//     name: "Doink",
+//     animal: "Cat",
+//     breed: "Mixed",
+//   },
+// ];
+
+const luna = {
+  name: "Luna",
+  animal: "Dog",
+  breed: "Havanesse",
+};
+
+const pepper = {
+  name: "Pepper",
+  animal: "Bird",
+  breed: "Cockateil",
+};
+
+const doink = {
+  name: "Doink",
+  animal: "Cat",
+  breed: "Mixed",
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanesse",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockateil",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    })
+  return (
+    <div>
+      <h1>Adopt Me</h1>
+      <Pet name={luna.name} animal={luna.animal} breed={luna.breed} />
+      <Pet name={pepper.name} animal={pepper.animal} breed={pepper.breed} />
+      <Pet name={doink.name} animal={doink.animal} breed={doink.breed} />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(React.createElement(App));
+root.render(<App />);
